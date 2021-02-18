1afa3ffd9146fe0d10035d15a18b295d7cc08cac
richardlee <18626685+richardlee159@users.noreply.github.com>
2021-02-18 13:21:05 +0800

    Fix xbus rdata routing

--- src/soc.v
+++ src/soc.v
@@ -12,7 +12,8 @@ wire        xbus_we;
 wire [3:0]  xbus_be;
 wire [31:0] xbus_addr;
 wire [31:0] xbus_wdata;
-wire [31:0] xbus_rdata;
+reg  [31:0] xbus_rdata;
+wire [31:0] xbus_slave_rdata [0:3];
 
 wire [`NSLAVES-1:0]xbus_cs;
 
@@ -21,6 +22,16 @@ xbus_decoder xbus_decoder(
     .xbus_addr (xbus_addr ),
     .xbus_cs   (xbus_cs   )
 );
+
+always @(*) begin
+    case (xbus_cs)
+        `NSLAVES'b0001: xbus_rdata = xbus_slave_rdata[0];
+        `NSLAVES'b0010: xbus_rdata = xbus_slave_rdata[1];
+        `NSLAVES'b0100: xbus_rdata = xbus_slave_rdata[2];
+        `NSLAVES'b1000: xbus_rdata = xbus_slave_rdata[3];
+        default: xbus_rdata = 0;
+    endcase
+end
 
 core #(
     .PC_RSTVAL  (32'h0000   )
@@ -43,7 +54,7 @@ ram ram(
     .xbus_be    (xbus_be    ),
     .xbus_addr  (xbus_addr  ),
     .xbus_wdata (xbus_wdata ),
-    .xbus_rdata (xbus_rdata )
+    .xbus_rdata (xbus_slave_rdata[0] )
 );
 
 endmodule
